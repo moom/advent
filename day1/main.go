1fb8eb173231052d3901f8593d258e4dd8bff62a
Felix Wang <fwang@fwang.local>
2021-12-02 09:10:59 +1100

    Day 1 challenge.

---
+++ day1/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+
+	i, p, c := 0, 0, 0
+
+	f, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		c, err= strconv.Atoi(s.Text())
+		if c > p && p>0 {
+			i++
+		}
+		p = c
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	fmt.Println(i)
+	if err = s.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
